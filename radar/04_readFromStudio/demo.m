9f22c18dbb093065a547bd8a6164280289046613
yyxr75 <yyxr164579504@gmail.com>
2022-08-12 10:26:13 +0800

    新增动态测角实验代码,demo2

--- radar/04_readFromStudio/demo.m
+++ radar/04_readFromStudio/demo.m
@@ -20,21 +20,29 @@ radar_cube_struct = rawDataReader(setup_filename, 'temp_raw_bin.bin', 'temp_cube
 %% 可调参数
 range_cfar_thresh = 0.25;
 doppler_cfar_thresh = 0.1;
-frame_number = 1;
-custome_peak_indx = 2;  % 调整这个值选取第几个峰为目标峰
+frame_number = 3;
+custome_peak_indx = 1;  % 调整这个值选取第几个峰为目标峰
 %% 开始
 frame = radar_cube_struct.data{frame_number};
 radar_cube_tx1 = frame(1:3:end,:,:);
 radar_cube_tx2 = frame(2:3:end,:,:);
 radar_cube_tx3 = frame(3:3:end,:,:);
 radar_cube_all = cat(2,radar_cube_tx1, radar_cube_tx2, radar_cube_tx3);
-figure(1);
+
 [chirps,atennas,range_all_indx] = size(radar_cube_all);
 range_coor = c/(2*radar_cube_struct.rfParams.bandwidth*1e9)*(0:range_all_indx-1);
 vel_coor = (-chirps/2:chirps/2)*radar_cube_struct.rfParams.dopplerResolutionMps;
 angle_reso=180;
 angle_coor = -angle_reso/2:angle_reso/2-1;
+%% 天线通道相位校准
+load attena_calib_vec.mat
+load attena_calib_ref.mat
+attena_calib_vec_norm = attena_calib_ref./attena_calib_vec;
+attena_calib_mat = repmat(attena_calib_vec_norm,[radar_cube_struct.rfParams.numDopplerBins,1,radar_cube_struct.rfParams.numRangeBins]);
+radar_cube_all = radar_cube_all.*attena_calib_mat;
+
 %% range fft
+figure(1);
 subplot(2,1,1);
 range = squeeze(sum(radar_cube_all(1,:,:),2));
 range_fft = 20*log10(squeeze(abs(range)));
@@ -107,7 +115,7 @@ for i = 1:radar_cube_struct.rfParams.numRangeBins
         bi_doppler_peak = cfar_ca1D_square(doppler_peak,12,6,doppler_cfar_thresh,0)==1;
         aoa_signal = radar_cube_all(bi_doppler_peak,:,i);
         aoa_result = musicAlg(aoa_signal',angle_reso);
-        AOA_heatmap(i,:) = aoa_result;        
+        AOA_heatmap(i,:) = aoa_result;
     end
 end
 
@@ -139,19 +147,82 @@ angle_spectrum = AOA_heatmap(custome_peak_indx,:);
 max_angle = angle_coor(max_angle_index);
 subplot(1,2,2);plot(max_angle,range_val,'ro');
 
+%% 多通道相位校正
+% figure(4);
+% chirp_channel_mat = squeeze(radar_cube_all(:,:,custome_peak_indx));
+% channel_mod = abs(chirp_channel_mat);
+% channel_phase = angle(chirp_channel_mat)*180/pi;
+% [chirps_number, channels_number] = size(chirp_channel_mat);
+% for i = 20:chirps_number
+%     subplot(1,2,1);
+%     plot(channel_mod(i,:));title('幅值分布');hold on
+%     subplot(1,2,2);
+%     plot(channel_phase(i,:));title('相位分布');hold on
+%     pause(0.1);
+% end
+% 
+% attena_calib_vec = mean(chirp_channel_mat,1);
+% attena_calib_ref = mean(chirp_channel_mat(:,1),1);
+% 
+% attena_calib_vec_norm = attena_calib_ref./attena_calib_vec;
+% attena_calib_mat = repmat(attena_calib_vec_norm,[chirps_number,1,radar_cube_struct.rfParams.numRangeBins]);
+% save attena_calib_vec.mat attena_calib_vec
+% save attena_calib_ref.mat attena_calib_ref
+% 
+% radar_cube_all_calib = radar_cube_all.*attena_calib_mat;
+% 
+% 
+% chirp_channel_mat = squeeze(radar_cube_all_calib(:,:,custome_peak_indx));
+% channel_mod = abs(chirp_channel_mat);
+% channel_phase = angle(chirp_channel_mat)*180/pi;
+% [chirps_number, channels_number] = size(chirp_channel_mat);
+% for i = 20:chirps_number
+%     subplot(1,2,1);
+%     plot(channel_mod(i,:));title('幅值分布');hold on
+%     subplot(1,2,2);
+%     plot(channel_phase(i,:));title('相位分布');hold on
+%     pause(0.01);
+% end
 
 %% 静态误差曲线
-gt_angle = [-45,-30:5:-5,5:5:30];
-fft_angle = [-44,-46,-46,-47,-49,-50,89,89,87,87,46,45,44];
-music_angle = [-32,-30,-30,-30,-2,-1,1,1,1,1,28,29,29];
-figure(4);
-subplot(2,1,1);
-plot(gt_angle,gt_angle,'ro-');hold on
-plot(gt_angle,fft_angle,'go-');hold on
-plot(gt_angle,music_angle,'bo-');hold on
-legend('gt','fft','music');
-subplot(2,1,2);
-% plot(fft_angle-gt_angle,'go-');hold on
-plot(gt_angle,music_angle-gt_angle,'bo-');hold on
-legend('music');
+% gt_angle = [-45,-30:5:-5,5:5:30];
+% fft_angle = [-44,-46,-46,-47,-49,-50,89,89,87,87,46,45,44];
+% music_angle = [-32,-30,-30,-30,-2,-1,1,1,1,1,28,29,29];
+% figure(4);
+% subplot(2,1,1);
+% plot(gt_angle,gt_angle,'ro-');hold on
+% plot(gt_angle,fft_angle,'go-');hold on
+% plot(gt_angle,music_angle,'bo-');hold on
+% legend('gt','fft','music');
+% subplot(2,1,2);
+% % plot(fft_angle-gt_angle,'go-');hold on
+% plot(gt_angle,music_angle-gt_angle,'bo-');hold on
+% legend('music');
+% 画位置图
+gt_range = 5;
+radar_range = 4.92;
+gt_angles = [-45,-30:5:-5,5:5:30];
+% fft_angles = [-44,-46,-46,-47,-49,-50,89,89,87,87,46,45,44];
+% music_angles = [-32,-30,-30,-30,-2,-1,1,1,1,1,28,29,29];
+
+fft_angles = [-44,-45,-46,-47,-89,-90,89,89,87,87,46,45,44];
+music_angles = [-32,-31,-30,-30,-2,-1,0,0,1,1,28,29,29];
+% data from TI
+% gt_angles = [+45,+40,+35,+30,+25,+20,+15,+10,+5,-5,-10,-15,-20,-25,-30,-35,-40,-45];
+% music_angles = [+60.5,+59.5,+20,+19.67,+19.75,+11,+11,+10.5,+7,+2,-3.833,-9.5,-8.25,-5.75,-41,-39.333,-53.5,-52];
+
+figure(5);
+for i = 1:length(gt_angles)
+    gt_angle = gt_angles(i);
+    gt_pos = [gt_range*sin(gt_angle/180*pi), gt_range*cos(gt_angle/180*pi)];
+    plot(gt_pos(1),gt_pos(2),'ro');hold on
+    fft_angle = fft_angles(i);
+    fft_pos = [radar_range*sin(fft_angle/180*pi), radar_range*cos(fft_angle/180*pi)];
+    plot(fft_pos(1), fft_pos(2),'go');hold on
+    music_angle = music_angles(i);
+    music_pos = [radar_range*sin(music_angle/180*pi), radar_range*cos(music_angle/180*pi)];
+    plot(music_pos(1), music_pos(2),'bo');hold on
+end
+legend('ground truth','fft','music');
+% legend('ground truth','music');
 hold off;
